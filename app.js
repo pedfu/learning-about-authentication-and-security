5f780d05a46356adc7eb7861024fa400629066b3
Pedro Fuziwara <fuziwarapedro@gmail.com>
2022-03-25 20:05:03 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/userDB')
+}
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, requireAuthenticationCode: false, encryptedFields: ['password']}); 
+//encrypt when save and dicrypt when find
+
+const User = mongoose.model('User', userSchema);
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save((err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({username: username}, (err, foundUser) => {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server started!')
+});
